c9075da77c940612e75075c05f2bcfc39a18a21d
damm1t <donatsokolov99@gmail.com>
2019-01-27 18:25:05 +0300

    fix function

--- function.h
+++ function.h
@@ -67,27 +67,23 @@ template<typename R, typename... Args>
 class function<R(Args...)> {
     using smallT =  std::array<char, MAX_SIZE>;
     using bigT = std::unique_ptr<callable<R, Args...>>;
-    using alligh = std::variant<bigT, smallT>;
+    using storage_t = std::variant<bigT, smallT>;
 public:
-    function() noexcept : call_v(nullptr) {}
+    function() noexcept : call_v() {}
 
-    function(std::nullptr_t) noexcept : call_v(nullptr) {}
+    function(std::nullptr_t) noexcept : call_v() {}
 
-    function(function const &other) : call_v(nullptr) {
+    function(function const &other) : call_v() {
         if (std::holds_alternative<smallT>(other.call_v)) {
-            if (std::holds_alternative<bigT>(call_v)) {
-                call_v = std::array<char, MAX_SIZE>();
-            }
+            call_v = std::array<char, MAX_SIZE>();
             reinterpret_cast<callable<R, Args...> *>(std::get<smallT>(other.call_v).data())->clone(
                     std::get<smallT>(call_v).data());
         } else if (std::get<bigT>(other.call_v)) {
             call_v = std::get<bigT>(other.call_v)->copy();
-        } else {
-            call_v = nullptr;
         }
     }
 
-    function(function &&other) noexcept : call_v(nullptr) {
+    function(function &&other) noexcept : call_v() {
         if (std::holds_alternative<bigT>(other.call_v)) {
             call_v.swap(other.call_v);
         } else {
@@ -98,11 +94,9 @@ public:
     }
 
     template<typename F>
-    function(F func) : call_v(nullptr) {
-        if constexpr (std::is_nothrow_move_constructible_v<F> && alignof(callable_impl<F, R, Args...>) <= alignof(alligh)){
-            if (std::holds_alternative<bigT>(call_v)) {
-                call_v = std::array<char, MAX_SIZE>();
-            }
+    function(F func) : call_v() {
+        if constexpr (std::is_nothrow_move_constructible_v<F> && alignof(callable_impl<F, R, Args...>) <= alignof(storage_t)){
+            call_v = std::array<char, MAX_SIZE>();
             new(std::get<smallT>(call_v).data()) callable_impl<F, R, Args...>(std::move(func));
         } else {
             call_v = std::make_unique<callable_impl<F, R, Args...>>(std::move(func));
@@ -126,22 +120,17 @@ public:
             return *this;
         }
         if (std::holds_alternative<bigT>(other.call_v)) {
-            bigT tmp(std::move(std::get<bigT>(other.call_v)));
-            if (std::holds_alternative<smallT>(call_v)) {
-                other.call_v = std::array<char, MAX_SIZE>();
-                reinterpret_cast<callable<R, Args...> *>(std::get<smallT>(call_v).data())->move_by_pointer(
-                        std::get<smallT>(other.call_v).data());
-            } else {
-                other.call_v = std::move(std::get<bigT>(call_v));
-            }
-            call_v = std::move(tmp);
-        } else {
             clear_small();
+            call_v = std::move(std::get<bigT>(other.call_v));
+        } else if(std::holds_alternative<smallT >(other.call_v)){
             if (std::holds_alternative<bigT>(call_v)) {
                 call_v = std::array<char, MAX_SIZE>();
             }
             reinterpret_cast<callable<R, Args...> *>(std::get<smallT>(other.call_v).data())->move_by_pointer(
                     std::get<smallT>(call_v).data());
+        }
+        else{
+            call_v = storage_t();
         }
         return *this;
     }
@@ -158,7 +147,7 @@ public:
         return std::holds_alternative<smallT>(call_v) || std::get<bigT>(call_v);
     }
 
-    R operator()(Args &&... args) {
+    R operator()(Args &&... args) const {
         if (std::holds_alternative<smallT>(call_v))
             return reinterpret_cast<callable<R, Args...> *>(std::get<smallT>(call_v).data())->call(
                     std::forward<Args>(args)...);
@@ -168,7 +157,7 @@ public:
 
 
 private:
-    mutable std::variant<bigT, smallT> call_v;
+    mutable storage_t call_v;
 
     void clear_small() {
         if (std::holds_alternative<smallT>(call_v)) {
